f61a26d7000a7b831c8d8aa9cdfce2f5c5f5bc52
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2022-02-01 00:01:04 -0800

    [CU-1ymt72g] Render simple triangle to the screen

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,5 +1,6 @@
-#define GL3W_IMPLEMENTATION
-#include <gl3w.h>
+// Game Systems
+#include <hpp/render_system.hpp>
+#include <hpp/shader_manager.hpp>
 
 // stlib
 #include <chrono>
@@ -7,20 +8,46 @@
 
 using Clock = std::chrono::high_resolution_clock;
 
-// Entry point
+// Triangle NDC
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
 int main() {
+	std::cout << "Starting The Fast and the Furryous" << std::endl;
 	// TO-DO:
 	// Global systems
 
-	// TO-DO
-	// Initializing window
+	// Initialize game systems
+	RenderSystem renderer = RenderSystem::RenderSystem();
+	GLFWwindow* window = renderer.getWindow(); // Window is part of the renderer context
 
-	// TO-DO:
-	// initialize the main systems
+	// Copy triangle vertex data into the vertex buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// TO-DO: 
-	// variable timestep loop
+	auto time = Clock::now();
 
-	std::cout << "Starting Fast and Furryous" << std::endl;
+	// Game loop
+	while (!glfwWindowShouldClose(window)) { // TO-DO: Make this loop condition depend on the world state, like in assignment template
+		// Process system events
+		glfwPollEvents();
+
+		// TO-DO: Create input system and manage external input here
+		// Note: GLFW is used to track keyboard & mouse input
+
+		// Determine time elapsed
+		auto now = Clock::now();
+		float elapsed_ms = (float)(std::chrono::duration_cast<std::chrono::microseconds>(now - time)).count() / 1000;
+		time = now;
+
+		renderer.draw(SHADER_PROGRAM_IDS::TRIANGLE);
+
+		glfwSwapBuffers(window);
+	}
+
+
+
 	return EXIT_SUCCESS;
 }
