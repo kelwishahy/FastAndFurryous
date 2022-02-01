8fa4db596e8431f6e23146ab004d42c72a6e881f
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2022-01-31 23:59:10 -0800

    [CU-1ymt72g] Added common functions and variables

---
+++ src/cpp/common.cpp
@@ -0,0 +1,37 @@
+#include <hpp/common.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../ext/stb_image/stb_image.h"
+
+bool glHasError() {
+	GLenum error = glGetError();
+
+	if (error == GL_NO_ERROR) return false;
+
+	while (error != GL_NO_ERROR) {
+		const char* error_str = "";
+		switch (error) {
+		case GL_INVALID_OPERATION:
+			error_str = "INVALID_OPERATION";
+			break;
+		case GL_INVALID_ENUM:
+			error_str = "INVALID_ENUM";
+			break;
+		case GL_INVALID_VALUE:
+			error_str = "INVALID_VALUE";
+			break;
+		case GL_OUT_OF_MEMORY:
+			error_str = "OUT_OF_MEMORY";
+			break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION:
+			error_str = "INVALID_FRAMEBUFFER_OPERATION";
+			break;
+		}
+
+		fprintf(stderr, "OpenGL: %s", error_str);
+		error = glGetError();
+		assert(false);
+	}
+
+	return true;
+}
---
+++ src/hpp/common.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <glad/glad.h>
+#include "../ext/project_path.hpp"
+#include <cassert>
+#include <iostream>
+#include <array>
+#include <vector>
+
+// glm
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+
+//Functions
+
+// Check for OpenGL errors
+bool glHasError();
+
+////////////////////////////////////////////////////////////////////////////////
+// Asset IDs
+////////////////////////////////////////////////////////////////////////////////
+
+enum class SHADER_PROGRAM_IDS {
+	CAT,
+	TRIANGLE,
+	TOTAL
+}; const int shaderProgramCount = (int)SHADER_PROGRAM_IDS::TOTAL;
+
+enum class TEXTURE_IDS {
+	CAT,
+	TRIANGLE,
+	TOTAL
+}; const int textureCount = (int)TEXTURE_IDS::TOTAL;
+
+enum class OBJECT_BUFFER_IDS {
+	CAT,
+	TRIANGLE,
+	TOTAL
+}; const int objectCount = (int)OBJECT_BUFFER_IDS::TOTAL;
+
+////////////////////////////////////////////////////////////////////////////////
+// Data Types
+////////////////////////////////////////////////////////////////////////////////
+
+struct TexturedVertex {
+	glm::vec3 position;
+	glm::vec2 texcoord;
+};
