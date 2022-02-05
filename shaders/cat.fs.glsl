323858dc56b602bbbd9793b5d1acd0850b37b093
Kareem El-Wishahy <kareem.elwishahy@gmail.com>
2022-02-05 11:26:47 -0800

    Discard transparent fragments in cat shader

--- shaders/cat.fs.glsl
+++ shaders/cat.fs.glsl
@@ -7,4 +7,9 @@ uniform sampler2D ourTexture;
 
 void main() {
     FragColor = texture(ourTexture, TexCoord);
+
+    // discard any fragments with an alpha of 0
+    if (FragColor.a == 0.0) {
+        discard;
+    }
 }
