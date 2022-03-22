8d7c5995e192932cfed00c8c078fc255217da96b
zhangfred8 <zhangfred8@gmail.com>
2022-03-21 19:10:23 -0700

    fixing another duplicated line

--- src/cpp/world_system.cpp
+++ src/cpp/world_system.cpp
@@ -425,10 +425,3 @@ void WorldSystem::remove_components() {
 	}
 
 }
-
-Game WorldSystem::level_one() {
-	Game level_one;
-	level_one.addCat(RIFLE, PLAYER_1_TEAM, { screenResolution.x / 2 - 200, screenResolution.y - 400 }, 100);
-
-	return level_one;
-}
